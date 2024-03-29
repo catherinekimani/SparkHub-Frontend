07a34d695b41f6a0713ac556a12e80941820fdfa
catherinekimani <catherinekimani882@gmail.com>
2024-03-30 00:14:28 +0300

    Set up Redux store with middleware and initial state

--- sparkhub/src/store.js
+++ sparkhub/src/store.js
@@ -3,7 +3,9 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { thunk } from "redux-thunk";
 import rootReducer from './reducers'
 
-const initialState = {};
+const initialState = {
+  content: [],
+};
 const middleware = [thunk];
 const store = createStore(
 	rootReducer,
